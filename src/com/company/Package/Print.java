50378cb878ab24b28348e43c26d86f788c419178
lixin <lixin1997210@outlook.com>
2021-12-16 00:04:24 +0800

    添加了list的输出方式

--- src/com/company/Package/Print.java
+++ src/com/company/Package/Print.java
@@ -2,9 +2,11 @@ package com.company.Package;
 
 import com.company.Utils.TrimClassName;
 
+import java.awt.*;
 import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 /*这个类存放各种数据的打印方法*/
 public class Print {
@@ -13,7 +15,14 @@ public class Print {
         Method method = this.getClass().getMethod(methodName, Object.class);
         method.invoke(this, obj);
     }
-
+    //输出List
+    public void PrintList(Object obj){
+        ArrayList<Object> list = new ArrayList<>();
+        if (obj instanceof ArrayList<?>){
+            list = (ArrayList<Object>)obj;
+        }
+        System.out.println(list.toString());
+    }
     //输出int[]
     public void PrintI(Object obj) {
         int[] ints = (int[]) obj;
--- src/com/company/Screen/Methods.java
+++ src/com/company/Screen/Methods.java
@@ -2,13 +2,47 @@ package com.company.Screen;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.company.Bean.TreeNode;
 
 /*这里存放所有的算法*/
 public class Methods {
-    public int[] levelOrder(TreeNode root) {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        //使用队列层序遍历
+        ArrayDeque<TreeNode> que = new ArrayDeque<>();
+        //一个节点出时要把它的左右子节点也放进去
+        if (root == null) {
+            return res;
+        }
+        que.offer(root);
+        int layer = 0;
+        while (!que.isEmpty()) {
+            List list = new ArrayList();
+            //每次把队列中的数取完即为取完了当前层
+            layer++;
+            int size = que.size();
+            for (int i = 0;i<size;i++){
+            TreeNode node = que.pollFirst();
+            list.add(node.val);
+            if (node.left != null) {
+                que.add(node.left);
+            }
+            if (node.right != null) {
+                que.add(node.right);
+            }
+            }
+            //如果layer是偶数,则反转链表
+            if (layer%2==0){
+                Collections.reverse(list);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+    public int[] levelOrder1(TreeNode root) {
         List list = new ArrayList<Integer>();
         //使用队列层序遍历
         ArrayDeque<TreeNode> que = new ArrayDeque<>();
